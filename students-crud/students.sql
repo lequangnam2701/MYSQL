460dc24f3b00c06ceb132225864ec2105614f302
lequangnam2701 <142825327+lequangnam2701@users.noreply.github.com>
2024-01-15 10:29:55 +0700

    Add files via upload

---
+++ students-crud/students.sql
@@ -0,0 +1,9 @@
+CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    email VARCHAR(255)
+);
+
+INSERT INTO students (name, email) VALUES
+    ('nam', 'lequangnam@gmail.com'),
+    ('linh', 'linh2005@ggmail.com');
